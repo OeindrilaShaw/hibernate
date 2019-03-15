d8320f7c7910c6c9d5ca550c6ca06db2936f6e45
OeindrilaShaw <46805563+OeindrilaShaw@users.noreply.github.com>
2019-03-15 14:15:32 +0530

    Add files via upload

---
+++ tablePerSubClassAnnot/src/com/pack/Contract_Employee.java
@@ -0,0 +1,28 @@
+package com.pack;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+@Entity
+@Table(name="Emp603")
+@PrimaryKeyJoinColumn(name="id")
+public class Contract_Employee extends Employee {
+private int pay_hour;
+private String contract_duration;
+public int getPay_hour() {
+	return pay_hour;
+}
+public void setPay_hour(int pay_hour) {
+	this.pay_hour = pay_hour;
+}
+public String getContract_duration() {
+	return contract_duration;
+}
+public void setContract_duration(String contract_duration) {
+	this.contract_duration = contract_duration;
+}
+
+}
---
+++ tableperclass/src/com/pack/Main.java
@@ -0,0 +1,42 @@
+package com.pack;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class Main {
+public static void main(String arg[])
+{
+	Configuration cfg=new Configuration().configure();
+	SessionFactory sf=cfg.buildSessionFactory();
+	Session s=sf.openSession();
+	Transaction t=null;
+	try
+	{
+		t=s.beginTransaction();
+		Employee e1=new Employee();
+		e1.setName("oeindrila");
+		Regular_Employee r=new Regular_Employee();
+		r.setSalary(50000);
+		r.setBonus(2000);
+		Contract_Employee c=new Contract_Employee();
+		c.setPay_hour(8);
+		c.setContract_duration("rfe");
+		s.persist(c);
+		s.persist(e1);
+		s.persist(r);
+		t.commit();
+		System.out.println("Succesfully inserted");
+	}
+	catch(HibernateException e)
+	{
+		System.out.println(e);
+	}
+	finally
+	{
+		s.close();
+	}
+}
+}
